095d205543b6c6e4c907107993807f91d2eed07a
이준구 <leejungoo1396@gmail.com>
2024-03-01 18:01:11 +0900

    Refactor. TodoForm.jsx에서 useState로 관리하던 input 태그 값을 useInput.js라는 커스텀 훅을 만들어 관리

--- src/component/main/TodoForm.jsx
+++ src/component/main/TodoForm.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import { createTodos } from "../../axios/api";
 import { useMutation, useQueryClient } from "react-query";
+import useInput from "hooks/useInput";
 
 function TodoForm() {
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
-  const [date, setDate] = useState("");
-  const [sort, setSort] = useState("");
+  const [title, onTitleChangeHandler, InitialTitle] = useInput();
+  const [content, onContentChangeHandler, initialContent] = useInput();
+  const [date, onDeadlineChangeHandler, initialDate] = useInput();
 
   const queryClient = useQueryClient();
   const { mutate, isPending, isError } = useMutation({
@@ -17,52 +17,22 @@ function TodoForm() {
     },
   });
 
-  const onTitleChangeHandler = (e) => {
-    setTitle(e.target.value);
-  };
-
-  const onContentChangeHandler = (e) => {
-    setContent(e.target.value);
-  };
-
-  const onDeadlineChangeHandler = (e) => {
-    setDate(e.target.value);
-  };
-
-  const onSortChangeHandler = (e) => {
-    const order = e.target.value;
-
-    setSort(order);
-
-    // setTodo((prev) => {
-    //   const sortedTodo = [...prev].sort((a, b) => {
-    //     if (order === "asc") {
-    //       return new Date(a.deadline) - new Date(b.deadline); // 오름차순 정렬
-    //     } else if (order === "desc") {
-    //       return new Date(b.deadline) - new Date(a.deadline); // 내림차순 정렬
-    //     }
-    //   });
-
-    //   return sortedTodo; // 정렬된 배열 반환
-    // });
-  };
-
   const setSubmit = (e) => {
     e.preventDefault();
 
     const newTodo = {
       id: crypto.randomUUID(),
-      title: title,
-      content: content,
+      title,
+      content,
       isDone: false,
       deadline: date.toString(),
     };
 
     mutate(newTodo);
 
-    setTitle("");
-    setContent("");
-    setDate("");
+    InitialTitle();
+    initialContent();
+    initialDate();
   };
   if (isPending) {
     //수행문
@@ -80,11 +50,6 @@ function TodoForm() {
         <input type="date" value={date} onChange={onDeadlineChangeHandler}></input>
         <button type="submit">추가하기</button>
       </form>
-      <select value={sort} onChange={onSortChangeHandler}>
-        <option value="default">정렬</option>
-        <option value="asc">오름차순</option>
-        <option value="desc">내림차순</option>
-      </select>
     </article>
   );
 }
--- src/component/main/TodoList.jsx
+++ src/component/main/TodoList.jsx
@@ -1,9 +1,30 @@
+import useInput from "hooks/useInput";
 import { useMutation, useQuery, useQueryClient } from "react-query";
 import styled from "styled-components";
-import { editTodos, fetchTodos, deleteTodos } from "../../axios/api";
+import { deleteTodos, editTodos, fetchTodos } from "../../axios/api";
 import TodoItem from "./TodoListItem";
 
 function TodoList() {
+  const [sort, onSortChangeHandler] = useInput();
+
+  // const onSortChangeHandler = (e) => {
+  //   const order = e.target.value;
+
+  //   setSort(order);
+
+  //   // setTodo((prev) => {
+  //   //   const sortedTodo = [...prev].sort((a, b) => {
+  //   //     if (order === "asc") {
+  //   //       return new Date(a.deadline) - new Date(b.deadline); // 오름차순 정렬
+  //   //     } else if (order === "desc") {
+  //   //       return new Date(b.deadline) - new Date(a.deadline); // 내림차순 정렬
+  //   //     }
+  //   //   });
+
+  //   //   return sortedTodo; // 정렬된 배열 반환
+  //   // });
+  // };
+
   const queryClient = useQueryClient();
 
   const { mutate: toggleMutate } = useMutation({
@@ -53,6 +74,11 @@ function TodoList() {
 
   return (
     <article className="todoList-area">
+      <select value={sort} onChange={onSortChangeHandler}>
+        <option value="default">정렬</option>
+        <option value="asc">오름차순</option>
+        <option value="desc">내림차순</option>
+      </select>
       <WorkingArea>
         <FontSize>Working</FontSize>
         <WorkingList>
